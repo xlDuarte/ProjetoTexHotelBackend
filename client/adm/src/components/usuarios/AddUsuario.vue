30359ee0d0fafafc664165f245241d3caf073557
Luizfe-nando <Luizfernandomoreirapinto@gmail.com>
2023-04-04 08:22:03 -0300

    responsividade usuarios

--- client/adm/src/components/usuarios/AddUsuario.vue
+++ client/adm/src/components/usuarios/AddUsuario.vue
@@ -4,39 +4,42 @@
       <form>
         <div class="container">
           <div class="row my-2">            
-            <div class="col col-5">
+            <div class="col col-sm-4 col-md-6">
             <label for="nome" class="mb-0 form-label fw-bold ">*Nome:</label>
             <input type="text"  v-model="nomeUser" class="form-control  w-100 "  />
-        </div>
-      
-      <div class="mx-auto col col-5">
-        <label for="email" class="mb-0 form-label fw-bold ">*E-mail:</label>
-        <input type="text"  v-model="emailUser" class="form-control  w-100 "  />        
-      </div>
-      <div class="col col-5">
-        <label for="cpf" class="mt-3 mb-0 form-label fw-bold ">*CPF:</label>
-        <input type="text"  v-model="cpfUser" class="form-control  w-100 "  /> 
-      </div>
-      <div class="mx-auto col col-5">        
-        <label for="endereço" class="mt-3 mb-0 form-label fw-bold ">*Endereço:</label>
-        <input type="text"  v-model="endUser" class="form-control  w-100 "  />
-      </div>
-      <div class="col col-5">
-        <label for="tipo" class="mt-3 mb-0 form-label fw-bold ">*Tipo:</label>
-        <input type="text"  v-model="tipoUser" class="form-control  w-100 "  />
-      </div>
-      <div class="mx-auto col col-5">
-        <label for="telefone" class="mt-3 mb-0 form-label fw-bold ">*Telefone:</label>
-        <input type="text"  v-model="telefoneUser" class="form-control  w-100 "  />
-      </div>
-      <div class="col col-5">
-        <label for="senha" class="mt-3 mb-0 form-label fw-bold ">*Senha:</label>
-        <input type="text"  v-model="senhaUser" class="form-control  w-100 "  />
-      </div>
-    </div>
-    <div class=" col col-5">
-      <button class="button mt-2" @click="saveUsuario">Cadastrar</button>
-    </div>
+            </div>  
+
+
+            <div class="mx-auto col col-sm-4 col-md-6">
+              <label for="email" class="mb-0 form-label fw-bold ">*E-mail:</label>
+              <input type="text"  v-model="emailUser" class="form-control  w-100 "  />        
+            </div>
+
+            <div class="col col-sm-4 col-md-6">
+              <label for="cpf" class="mt-3 mb-0 form-label fw-bold ">*CPF:</label>
+              <input type="text"  v-model="cpfUser" class="form-control w-100 "  /> 
+            </div>
+
+            <div class="mx-auto col col-sm-4 col-md-6">        
+              <label for="endereço" class="mt-3 mb-0 form-label fw-bold ">*Endereço:</label>
+              <input type="text"  v-model="endUser" class="form-control w-100 "  />
+            </div>
+            <div class="col col-sm-4 col-md-6">
+              <label for="tipo" class="mt-3 mb-0 form-label fw-bold ">*Tipo:</label>
+              <input type="text"  v-model="tipoUser" class="form-control  w-100 "  />
+            </div>
+            <div class="mx-auto col col-sm-4 col-md-6">
+              <label for="telefone" class="mt-3 mb-0 form-label fw-bold ">*Telefone:</label>
+              <input type="text"  v-model="telefoneUser" class="form-control  w-100 "  />
+            </div>
+            <div class="col col-sm-4 col-md-6">
+              <label for="senha" class="mt-3 mb-0 form-label fw-bold ">*Senha:</label>
+              <input type="text"  v-model="senhaUser" class="form-control  w-100 "  />
+            </div>
+          </div>
+          <div class=" col col-sm-4 col-md-6">
+            <button class="button mx-auto mt-2" @click="saveUsuario">Cadastrar</button>
+          </div>
         </div>
       </form>
     </div>
@@ -94,40 +97,6 @@ export default {
   padding: 2vw;
   transition: all 0.3s ease;
   color: black;
-}
-
-.sec>div {
-  max-width: 90%;
-  margin: 2% 5%;
-}
-
-.flex {
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 90%;
-  margin: 0 5%;
-}
-
-.flex>div {
-  flex: 1 1 420px;
-  margin: 0 5%;
-}
-
-.cadinput {
-  display: inline-block;
-  width: 200px;
-  margin: 5px;
-}
-
-.cadlabel {
-  display: inline-block;
-  width: 130px;
-  margin-right: 15px;
-  font-size: 1.2em;
-}
-
-.cadtextarea {
-  width: 310px;
 }
 
 .button {
