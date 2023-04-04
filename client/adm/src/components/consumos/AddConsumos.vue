06b5b413109e2700d1b77051c1b0f884f51400d4
FelipeSousa366 <felipe36603466@gmail.com>
2023-04-04 09:54:32 -0300

    Deixando Consumo responsivo

--- client/adm/src/components/consumos/AddConsumos.vue
+++ client/adm/src/components/consumos/AddConsumos.vue
@@ -6,11 +6,11 @@
       <form>
         <div class="container">
           <div class="row my-2">
-        <div class="col col-5 me-5">
+        <div class="col col-sm-4 col-md-6">
           <label for="roomNumber" class="mb-0 form-label fw-bold ">*Número do quarto:</label>
           <input type="number" id="quarto" name="Quarto" class="form-control" v-model="quarto" />
         </div>
-        <div class="col col-5 me-5">
+        <div class="col col-sm-4 col-md-6">
           <label for="local" class="mb-0 form-label fw-bold ">*Local do Consumo</label>
           <select id="localDeConsumo" class="form-control" v-model="localConsumo" @change="updateProdutos">
             <option value='1'>Frigobar</option>
@@ -18,7 +18,7 @@
             <option value='3'>Bar</option>
           </select>
           </div>
-          <div class="mt-3 col col-5 me-5">
+          <div class="mt-3 mx-auto col col-sm-4 col-md-6">
           <label for="produtoNumber" class="mb-0 form-label fw-bold ">*Produto</label>
           <select id="produto" class="form-control" v-model="idproduto">
             <option v-for="produto in filteredProdutos" :value="produto.id" v-bind:key="produto.id">
@@ -28,11 +28,11 @@
           </div>
         
 
-        <div class="mt-3 col col-5 me-5">
+        <div class="mx-auto mt-3 col col-sm-4 col-md-6">
           <label for="quantity" class="mb-0 form-label fw-bold ">*Quantidade:</label>
           <input type="text" id="quantidade" name="quantidade" class="form-control" v-model="quantidade" />
         </div>
-        <div class="mt-3 col col-5 me-5">
+        <div class="mt-3 col col-sm-4 col-md-6">
           <label for="date" class="mb-0 form-label fw-bold ">*Data:</label>
           <input type="datetime-local" id="inputDateTime" class="form-control" v-model="data" />
         </div>
